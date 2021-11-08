5cc8f7e2667627438f75c828495d47803f7392b8
W. Guilherme <ravok123@gmail.com>
2021-11-08 11:34:22 -0300

    [update] setup eslint + prettier configs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";;
+
+const lek = 1;
+
 
 export default function Home() {
   return (
